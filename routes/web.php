02250a13a56a9de661659d9d77761452d80a5157
Shivang <32.shivangdubey@gmail.com>
2022-11-17 00:10:22 +0530

    removed test mail route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/mail', function () {
-    return new App\Mail\OrderConfirmed(6);
-});
-
 Route::get('/{any?}', [
     function () {
         return view('index');
