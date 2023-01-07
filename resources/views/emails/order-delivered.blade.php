02a3d4158c167953559181cdfd5504cb8634e5bd
Shivang <32.shivangdubey@gmail.com>
2023-01-07 18:13:47 +0530

    remove unused backend code

--- resources/views/emails/order-delivered.blade.php
+++
@@ -1,7 +0,0 @@
-<section style="max-width: 72ch; margin: 0 auto; padding: 0.5rem;">
-    <h1>
-        Order Delivered
-    </h1>
-    <p>Your order at AnonymousToys has been delivered. We hope you don't have any complaints.</p>
-    <p>Thank You for choosing us!</p>
-</section>
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\AdminController;
 use App\Http\Controllers\Auth\StaffController;
-use App\Http\Controllers\CarouselImageController;
-use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\Choices\ProductFieldChoices;
-use App\Http\Controllers\ContactMessageController;
-use App\Http\Controllers\CustomerReviewController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -46,39 +40,6 @@ Route::prefix("/user")->group(function () {
     Route::get('/staff/{user}', [StaffController::class, 'show']);
 });
 
-Route::prefix('/checkout')->group(function () {
-    Route::post('', [CheckoutController::class, 'store']);
-    Route::post('/pay', [CheckoutController::class, 'update']);
-});
-
-Route::prefix('/order')->group(function () {
-    Route::get('/all', [OrderController::class, 'index']);
-    Route::get('/{id}', [OrderController::class, 'show']);
-    Route::post('/update/{order}', [OrderController::class, 'update']);
-});
-
-Route::prefix('/contact')->group(function () {
-    Route::post('/create', [ContactMessageController::class, 'store']);
-    Route::get('/all', [ContactMessageController::class, 'index']);
-    Route::get('/{message}', [ContactMessageController::class, 'show']);
-    Route::delete('/{message}', [ContactMessageController::class, 'destroy']);
-});
-
-Route::prefix('/review')->group(function () {
-    Route::post('/create', [CustomerReviewController::class, 'store']);
-    Route::get('/all', [CustomerReviewController::class, 'index']);
-    Route::get('/random', [CustomerReviewController::class, 'indexRandom']);
-    Route::post('/update/{review}', [CustomerReviewController::class, 'update']);
-    Route::delete('/{review}', [CustomerReviewController::class, 'destroy']);
-});
-
-Route::prefix('/post')->group(function () {
-    Route::post('/create', [PostController::class, 'store']);
-    Route::get('/all', [PostController::class, 'index']);
-    Route::delete('/{id}', [PostController::class, 'destroy']);
-    Route::get('/{id}', [PostController::class, 'show']);
-});
-
 Route::prefix('/product')->group(function () {
     Route::get('/all', [ProductController::class, 'index']);
     Route::get('/random', [ProductController::class, 'indexRandom']);
@@ -86,13 +47,6 @@ Route::prefix('/product')->group(function () {
     Route::post('/create', [ProductController::class, 'store']);
     Route::post('/update/{product}', [ProductController::class, 'update']);
     Route::delete('/{product}', [ProductController::class, 'destroy']);
-});
-
-Route::prefix('/carousel')->group(function () {
-    Route::get('', [CarouselImageController::class, 'index']);
-    Route::post('/create', [CarouselImageController::class, 'store']);
-    Route::post('/update/{item}', [CarouselImageController::class, 'update']);
-    Route::delete('/{item}', [CarouselImageController::class, 'destroy']);
 });
 
 
